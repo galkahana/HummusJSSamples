e13adca9a20a2e6d7352d493a463f389b29a0722
galkahana <gal.kahana@hotmail.com>
2017-01-22 22:06:25 +0200

    translating pass + toUnicode

--- text-extraction/test.js
+++ text-extraction/test.js
@@ -2,20 +2,20 @@ var hummus = require('hummus');
 var _ = require('lodash');
 var extractText = require('./text-extraction');
 
-function placementToDisplay(objectPlacements) {
-    return _.map(objectPlacements,
-            (placement)=> {
-                if(placement.type === 'text')
-                    return _.map(placement.text,(item)=> {return {text:item.text.asText/*,bytes:item.text.asBytes*/,font:item.textState.font};});
-                else 
-                    return placement.objectId;
-            });
-}
-
 function runMe() {
     var pdfReader = hummus.createReader('./samples/HighLevelContentContext.PDF');
     
     var {pagesPlacements,formsPlacements} = extractText(pdfReader);
+
+    function placementToDisplay(objectPlacements) {
+        return _.map(objectPlacements,
+                (placement)=> {
+                    if(placement.type === 'text')
+                        return _.map(placement.text,(item)=> {return {text:item.text};});
+                    else 
+                        return placement.objectId;
+                });
+    }
 
     console.log('pages text placements',JSON.stringify(_.map(pagesPlacements,placementToDisplay),null,2));
     console.log('forms text Placements',JSON.stringify(_.mapValues(formsPlacements,placementToDisplay),null,2));
